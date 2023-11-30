f1238430c1513e766380a0b3389759205a1d9755
Tevin Thuku <tevinthuku@gmail.com>
2023-11-30 07:15:05 +0300

    add tests for the partition workflow

--- partitions/diesel_pg_table_partitions/README.md
+++ partitions/diesel_pg_table_partitions/README.md
@@ -7,22 +7,23 @@ diesel migration run
 ## Creating a partition
 
 ```
-cargo run -- create-partition november_2023 2023-11-01 2023-11-30
+cargo run -- create-partition sales_december_2023 2023-12-01 2023-12-31
 ```
 
 ## inserting data
+
 ```
-cargo run -- new-sale 1 2023-11-02 200
+cargo run -- new-sale 1 2023-12-02 200
 ```
 
 ## Querying data
-```
-cargo run -- read-sales 2023-11-01 2023-11-30
-```
 
+```
+cargo run -- read-sales 2023-12-01 2023-11-30
+```
 
 ## Analyze the query plan
 
 ```
-EXPLAIN ANALYZE SELECT * FROM sales WHERE (sales.sale_date BETWEEN '2023-11-01' AND '2023-11-30')
+EXPLAIN ANALYZE SELECT * FROM sales WHERE (sales.sale_date BETWEEN '2023-12-01' AND '2023-12-30')
 ```
--- partitions/diesel_pg_table_partitions/src/main.rs
+++ partitions/diesel_pg_table_partitions/src/main.rs
@@ -150,3 +150,53 @@ async fn read_sales(pool: DieselPool, from: String, to: String) -> anyhow::Resul
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use connection_pool::test_fixtures::diesel_pool_fixture;
+    use connection_pool::DieselPool;
+    use diesel::pg::Pg;
+    use diesel_migrations::{
+        embed_migrations, EmbeddedMigrations, HarnessWithOutput, MigrationHarness,
+    };
+    use rstest::*;
+
+    use crate::{create_partition, new_sale};
+
+    const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
+    const PARTITION_START_DATE: &str = "2023-12-01";
+    const PARTITION_END_DATE: &str = "2023-12-31";
+
+    // the SALE_DATE must "fit" in a partition, otherwise an error will be thrown by postgres.
+    const SALE_DATE: &str = "2023-12-02";
+
+    fn run_migrations(connection: &mut impl MigrationHarness<Pg>) {
+        HarnessWithOutput::write_to_stdout(&mut *connection)
+            .run_pending_migrations(MIGRATIONS)
+            .unwrap();
+    }
+
+    #[fixture]
+    pub async fn diesel_pool_with_single_partition(diesel_pool_fixture: DieselPool) -> DieselPool {
+        let mut conn = diesel_pool_fixture.get().unwrap();
+        run_migrations(&mut conn);
+        create_partition(
+            diesel_pool_fixture.clone(),
+            "sales_december_partition".to_owned(),
+            PARTITION_START_DATE.to_owned(),
+            PARTITION_END_DATE.to_owned(),
+        )
+        .await
+        .unwrap();
+
+        diesel_pool_fixture
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn test_making_a_new_sale_works(#[future] diesel_pool_with_single_partition: DieselPool) {
+        let pool = diesel_pool_with_single_partition.await;
+        let result = new_sale(pool, 1, SALE_DATE.to_owned(), 200).await;
+        assert!(result.is_ok())
+    }
+}
